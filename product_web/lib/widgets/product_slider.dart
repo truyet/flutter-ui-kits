99d1462858eed5bcb27c6cdeb6a7bf5ab1fc1757
Olayemii <donyemisco@gmail.com>
2021-01-26 13:15:38 +0100

    add chevrons

--- product_web/lib/widgets/product_slider.dart
+++ product_web/lib/widgets/product_slider.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:product_web/utils/colors.dart';
 
 class ProductSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 160.0,
+      width: MediaQuery.of(context).size.width * 0.75,
       margin: EdgeInsets.only(bottom: 50.0, top: 30.0),
-      color: Colors.red,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [],
+        children: [
+          Container(
+            width: 40.0,
+            child: Icon(
+              FlutterIcons.chevron_left_fea,
+              color: kTextDarkColor.withOpacity(0.5),
+            ),
+          ),
+          Expanded(
+            child: SizedBox(),
+          ),
+          Container(
+            width: 40.0,
+            child: Icon(
+              FlutterIcons.chevron_right_fea,
+              color: kTextDarkColor.withOpacity(0.5),
+            ),
+          ),
+        ],
       ),
     );
   }
