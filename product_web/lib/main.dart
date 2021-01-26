6ac866c19aba6e1602840d7dc0f0caa432d9ecc5
Olayemii <donyemisco@gmail.com>
2021-01-26 10:52:38 +0100

    header widget

--- product_web/lib/main.dart
+++ product_web/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:product_web/pages/home.dart';
 
 void main() {
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.interTextTheme(),
+      ),
       home: Navigator(
         pages: [
           MaterialPage(
